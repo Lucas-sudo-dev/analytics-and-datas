4d549293470aea4f3f722463673ea4f7b38ac2bc
Lucas Bomfim da Silva <lucasbomfimdasilva@gmail.com>
2022-08-07 21:01:12 -0300

    Adicionando o arquivo de create-database.sql

---
+++ database/mysql/create-database.sql
@@ -0,0 +1,20 @@
+/*
+No banco de dados MySQL Server, caso você queira criar um
+database ou schema, saiba que ambos são sinônimos ou seja
+significam a mesma coisa.
+*/
+
+-- 1: COMANDO/SYNTAX
+CREATE DATABASE NOM_DO_DB;
+
+
+-- 2: COMANDO/SYNTAX
+CREATE SCHEMA NOM_DO_SCHEMA;
+
+
+
+-- 3: COMANDO/SYNTAX
+/*
+Comando para criar o database/schema caso ele não exista
+*/
+CREATE DATABASE IF NOT EXISTS NOM_DO_DB;
